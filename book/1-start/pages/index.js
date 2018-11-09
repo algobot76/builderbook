306c0c193a9f653aabafae419af12a98ffb4378e
Kaitian Xie <xkaitian@gmail.com>
2018-11-08 22:46:22 -0800

    Update index page

--- book/1-start/pages/index.js
+++ book/1-start/pages/index.js
@@ -1,4 +1,13 @@
-[1, 2, 3].map((x) => {
-  const y = x + 1;
-  return x * y;
-});
+import Head from 'next/head';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
+    </Head>
+    <p>Content on Index page</p>
+  </div>
+);
+
+export default Index;
